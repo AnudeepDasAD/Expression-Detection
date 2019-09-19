3adf841fc338f85654084bc132aa5c80d053e9bd
AnudeepDasAD <55476249+AnudeepDasAD@users.noreply.github.com>
2019-09-19 01:38:58 -0400

    Update expressions.py

--- expressions.py
+++ expressions.py
@@ -1,3 +1,6 @@
+#Expressions.py organizes the training and testing data for the model by iterating through the file
+#	structure within the Cohn-Kanade labelled image file system
+
 #Need to collect images first
 import cv2
 import os
@@ -21,7 +24,6 @@ BASE_DIR  = os.path.dirname(os.path.abspath(__file__))
 
 #Actual folder names
 labels_directory = "Emotion"
-
 images_directory= "cohn-kanade-images"
 
 #label will be the folder's name
@@ -39,59 +41,52 @@ y_labels = []
 paths = []
 
 #Walking through the directory and assembling all of the y-values
+#.txt files contain the labels of the images, there is a number in each text file that corresponds to a specific emotion, 
+#	as specified in the dictionary
 for root, dirs, files in os.walk(labels_directory):
 	for file in files:
 		if file.endswith("txt"):
 			path = os.path.join(root, file)
 			paths.append(path)
-			#f = open(str(path), "r")
 			with open(str(path)) as f:
 				for line in f:
 					line = line.strip()
 					#Line is not empty
 					if line:
-						#print(int(float(line))) It works!
+						#Adding the label to the list of labels
 						id_ = int(float(line))
 						y_labels.append(id_)
-						#print(id_)
 
 #Assembling the images and x_train
 
-index = 0
+
 for path in paths:
-	#The first part of the relative path changes
+	#The first part of the relative path changes for the actual images 
 	path = path.replace(labels_directory, images_directory)
 
-	#The name of 
 	path = path.replace("_emotion.txt", ".png")
 
 	pil_image = Image.open(path).convert("L")	 #grayscale
 
-	#pil_image.resize((275,275), Image.ANTIALIAS)
+	
 	#Convert the image into a numpy array
 	image_array = np.array(pil_image, "uint8")
 
-	#print(image_array.shape)
 
 	#Ensuring all have the same shape
 	if image_array.shape != (490,640):
 		y_labels.pop(index)
 		continue
 
-	#Add the faces
+	#Add the faces to the training image list after they are detected
 	faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.3, minNeighbors=5)
 	for (x,y,w,h) in faces:
 		region_of_interest = image_array[y:y+h, x:x+w]
-
-		#np.reshape(region_of_interest, (250,250))
-		print(region_of_interest.shape)
-		#print(y_labels)
-
 		x_train.append(region_of_interest)
-	index += 1
+	
 #Both are 327 elements large
-print(len(x_train))
-print(len(y_labels))
+#print(len(x_train))
+#print(len(y_labels))
 
 
 #Go through all of the images and process them into our x_labels
